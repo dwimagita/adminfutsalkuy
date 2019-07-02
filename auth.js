d95123f6e23bfc01146d998aa364f10d41a2f239
dwima <dwima97@gmail.com>
2019-07-02 22:35:03 +0700

    second commit

--- auth.js
+++ auth.js
@@ -1,19 +1,56 @@
-function login(){
-// window.alert("working@!!!!")
-  var userEmail = document.getElementById("inputEmail").value;
-  var userPass = document.getElementById("inputPassword").value;
+
+// (function(){
   
-   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-     // Handle Errors here.
+//   firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//     window.location.href = "index.html"
+//   } else {
+//     // No user is signed in.
+//   }
 
-   console.log('user', firebase.auth().currentUser  )
+// });
+// })()
 
-     var errorCode = error.code;
-     var errorMessage = error.message;
 
-     window.alert("Error : " + errorMessage);
+function login(){
+  // window.alert("working@!!!!")
+    var userEmail = document.getElementById("inputEmail").value;
+    var userPass = document.getElementById("inputPassword").value;
+    
+     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function(user) {
+       setSession()
+      window.location.href = "index.html";
+      // user signed in
+   }).catch(function(error) {
+       // Handle Errors here.
+  
+       var errorCode = error.code;
+       var errorMessage = error.message;
+  
+       window.alert("Error : " + errorMessage);
+  
+       // ...
+     });
 
-     // ...
-   });
+  }
+  function setSession(){
+    console.log("masuk session")
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .then(function() {
+      console.log("masuk")
+      // Existing and future Auth states are now persisted in the current
+      // session only. Closing the window would clear any existing state even
+      // if a user forgets to sign out.
+      // ...
+      // New sign-in will be persisted with session persistence.
+      // return firebase.auth().signInWithEmailAndPassword(email, password);
+      return login();
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
 
-}
+  }
--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var user = firebase.auth().currentUser;
+var indexApp = {};
+(function(){
+  var firebase = app_fireBase;
+  var uid = null;
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user != null) {
+      // User is signed in.
 
-    if(user != null){
-
+      var user = firebase.auth().currentUser;
+      uid = user.uid;
       var email_id = user.email;
-      document.getElementById("namalogin").innerHTML = "Welcome User : " + email_id;
-
+      document.getElementById("namalogin").innerHTML = "SELAMAT DATANG : " + email_id;
+      console.log("masuk index")      
+    } else {
+      // console.log("redirect")
+      // No user is signed in.
+      window.location.href = "login.html";
     }
+  });
+
+})()
+
+function logOut(){
+  console.log('masuk logout')
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    window.location.href = "login.html";
+  }).catch(function(error) {
+    // An error happened.
+  });
   
-  } else {
-      window.location = "login.html";
-  }    
-});  
+}
